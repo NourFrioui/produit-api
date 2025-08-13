1f7d8f6bebe12204ade893b221951b375a74a9e4
nourfrioui <nourfrioui88@gmail.com>
2025-08-13 15:02:54 +0100

    Initial commit

---
+++ src/test/java/com/springboot_test/stock_management/StockManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_test.stock_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
